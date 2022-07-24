af9eaf963db58daab1d3401427de146c83df4c60
Michael McAuliffe <michael.e.mcauliffe@gmail.com>
2022-07-24 09:07:37 -0700

    Add windows build

---
+++ recipe/bld.bat
@@ -0,0 +1,20 @@
+@echo on
+
+mkdir build
+cd build
+
+set "LIBRARY_PREFIX=%LIBRARY_PREFIX:\=/%"
+
+cmake -GNinja ^
+    -DCMAKE_BUILD_TYPE=Release ^
+    -DCMAKE_INSTALL_PREFIX="%LIBRARY_PREFIX%" ^
+    -DCMAKE_PREFIX_PATH="%LIBRARY_PREFIX%" ^
+    -DBUILD_SHARED_LIBS=ON ^
+    ..
+if %ERRORLEVEL% neq 0 exit 1
+
+cmake --build . --verbose --config Release -- -v -j %CPU_COUNT%
+if %ERRORLEVEL% neq 0 exit 1
+
+cmake --install . --verbose --config Release
+if %ERRORLEVEL% neq 0 exit 1
