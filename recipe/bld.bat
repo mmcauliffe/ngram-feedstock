c1c936b59204610698e4c7c443ad92b3a47eca4b
Michael McAuliffe <michael.e.mcauliffe@gmail.com>
2022-08-03 18:08:22 -0700

    Update CMake patch

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -4,16 +4,23 @@ mkdir build
 cd build
 
 set "LIBRARY_PREFIX=%LIBRARY_PREFIX:\=/%"
+set "OLDPATH=%PATH%"
 
 cmake -GNinja ^
     -DCMAKE_BUILD_TYPE=Release ^
     -DCMAKE_INSTALL_PREFIX="%LIBRARY_PREFIX%" ^
     -DCMAKE_PREFIX_PATH="%LIBRARY_PREFIX%" ^
     -DBUILD_SHARED_LIBS=ON ^
+    -DNGRAM_BUILD_TEST=OFF ^
     ..
 if %ERRORLEVEL% neq 0 exit 1
 
 cmake --build . --verbose --config Release -- -v -j %CPU_COUNT%
+if %ERRORLEVEL% neq 0 exit 1
+
+set "PATH=%LIBRARY_PREFIX%\lib;%SRC_DIR%\build\src\lib;%SRC_DIR%\build\src\test;%PATH%"
+ctest
+set "PATH=%OLDPATH%"
 if %ERRORLEVEL% neq 0 exit 1
 
 cmake --install . --verbose --config Release
